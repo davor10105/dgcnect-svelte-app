e840efdb2c5574dc98247a46c4ed60cdc11f8fcc
Davor Vukadin <davor10105@gmail.com>
2023-11-07 09:17:28 +0000

    added configs for hosting

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		port: 5000,
+	},
+	preview: {
+		port: 5000,
+	},
 	plugins: [sveltekit()]
 });
